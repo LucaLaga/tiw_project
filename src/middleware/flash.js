7a33461e2b9f76f3fb79103b2411b43a651dc380
LucaLaga <lucalagana998@gmail.com>
2026-09-05 10:06:29 +0200

    feat: update flash middleware to use structured objects

--- src/middleware/flash.js
+++ src/middleware/flash.js
@@ -1,7 +1,14 @@
 function flashMiddleware(req, res, next) {
   if (req.session && req.session.flash) {
-    res.locals.flash = req.session.flash;
+    const flash = req.session.flash;
+    res.locals.flash = {
+      success: flash.type === 'success' ? flash.message : null,
+      error: flash.type === 'error' ? flash.message : null,
+      info: flash.type === 'info' ? flash.message : null,
+    };
     delete req.session.flash;
+  } else {
+    res.locals.flash = { success: null, error: null, info: null };
   }
   next();
 }
